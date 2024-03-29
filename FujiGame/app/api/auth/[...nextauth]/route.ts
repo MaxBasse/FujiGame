33c48e37c76e03de3dd29671a481e259e8c814ba
Max <maxxbasse@gmail.com>
2024-03-29 13:03:10 +0100

    login check back on

--- FujiGame/app/api/auth/[...nextauth]/route.ts
+++ FujiGame/app/api/auth/[...nextauth]/route.ts
@@ -14,8 +14,7 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async signIn({ account, profile }) {
       if (account?.provider === "google") {
-        return true
-        //return  (profile?.email?.endsWith("icam.fr") || profile?.email?.endsWith("maxent.brunel@gmail.com")) ? true : "/error";
+        return  (profile?.email?.endsWith("icam.fr") || profile?.email?.endsWith("maxent.brunel@gmail.com")) ? true : "/error";
       }
       
       return "/error"
