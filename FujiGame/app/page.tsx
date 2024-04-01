57e9106393d2b1cfee4c852a6b86e7d94e042f81
Maxent BRUNEL <maxx.basse@gmail.com>
2024-04-01 19:30:26 +0200

    zboub

--- FujiGame/app/page.tsx
+++ FujiGame/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
   const isLogged = session?.user?.email !== undefined && session?.user?.email !== null;
   const date = new Date();
-  const closingDate = new Date("2024-04-01T12:15:00Z");
+  const closingDate = new Date("2024-04-03T16:30:00Z");
   var hide = true;
 
   if((session?.user?.email?.endsWith("maxent.brunel@gmail.com") || session?.user?.email?.endsWith("eliot.maignan@2027.icam.fr")) || date.getTime() < closingDate.getTime()) hide = false;
